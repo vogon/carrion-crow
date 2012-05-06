23c5b65a67efd1428472c3d51a07e8bff19a4e67
patrick <patrick@studybreak.com>
2012-05-05 19:55:31 -0700

    Pools and shit.

--- userpool.rb
+++ userpool.rb
@@ -1,15 +1,29 @@
+require 'yaml'
+
 class UserPool
     def initialize
-        @pool = []
-        @cursor = 0
+        parts = YAML.load_file('users.yml')
+        @pool = parts[:pool]
+        @cursor = parts[:cursor]
+    end
+
+    def flush
+        obj = {:pool=>@pool, :cursor=>@cursor}
+        File.open('users.yml', 'w') do |out|
+            YAML.dump(obj, out)
+        end
     end
 
     def add(username)
-        @pool << username
+        if !@pool.include? username
+            @pool << username
+            flush
+        end
     end
 
     def next!
         @cursor = (@cursor + 1) % @pool.length
+        flush
     end
 
     def current
